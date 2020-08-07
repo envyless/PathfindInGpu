2e17b244f8bd6721a5ad130b254d11b28aa74fdd
envyless <yym4789@hotmail.com>
2020-08-07 21:31:00 +0900

    fix common utils bug

--- AstarWithGPU/Assets/CommonUtils/LSLogger.cs
+++ AstarWithGPU/Assets/CommonUtils/LSLogger.cs
@@ -1,4 +1,7 @@
- public static class LSLogger
+using System.Diagnostics;
+using UnityEngine;
+
+public static class LSLogger
  {
      [Conditional("QA"), Conditional("TEST"), Conditional("UNITY_EDITOR")]
      public static void Log(string logString, Color? color = null)
@@ -6,12 +9,12 @@
          if (color == null)
              color = Color.white;
          
--        Debug.Log("<color=#"+ColorUtility.ToHtmlStringRGB(color.Value)+">"+logString+"</color>");
-+        Debug.Log("<color=#"+ColorUtility.ToHtmlStringRGB(color.Value)+"> "+logString+" </color>");
+        UnityEngine.Debug.Log("<color=#"+ColorUtility.ToHtmlStringRGB(color.Value)+">"+logString+"</color>");
+        UnityEngine.Debug.Log("<color=#"+ColorUtility.ToHtmlStringRGB(color.Value)+"> "+logString+" </color>");
      }
  
      public static void LogError(string logString)
      {
-         Debug.LogError(logString);
+        UnityEngine.Debug.LogError(logString);
      }
  }
