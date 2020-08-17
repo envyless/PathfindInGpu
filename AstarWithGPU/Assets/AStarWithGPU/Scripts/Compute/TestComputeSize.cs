8a588343b28f2cc9b844936c0881ab4ecf80c130
envyless <yym4789@hotmail.com>
2020-08-18 00:28:45 +0900

    temp

---
+++ AstarWithGPU/Assets/AStarWithGPU/Scripts/Compute/TestComputeSize.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestComputeSize : MonoBehaviour
+{
+    struct MT
+    {
+        int a;
+        int b;
+        int c;
+        int d;/*
+        int f;
+        int g;
+        int h;
+        int i;*/
+
+    };
+
+    MT[] mt;
+
+    // Start is called before the first frame update
+    public ComputeShader cs;
+    public int kernelID = 0;
+
+    ComputeBuffer cb;
+
+    const int size = 10000;
+
+    void Start()
+    {
+        mt = new MT[size];
+        for(int i = 0; i < size; ++i)
+        {
+            mt[i] = new MT();
+        }
+
+        Debug.LogError("byte size of array : " + mt.GetByteSize<MT>() * CommonUtils.Btye2Mb);
+
+        kernelID = cs.FindKernel("MTestFunc");
+        cb = new ComputeBuffer(size, CommonUtils.GetByteSize<MT>());
+        cb.SetData(mt);
+        cs.SetBuffer(kernelID, "cb", cb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cs.Dispatch(kernelID, size, 1, 1);
+        cb.GetData(mt);
+    }
+
+    private void OnDestroy()
+    {
+        cb.Release();
+    }
+}
---
+++ AstarWithGPU/Assets/CommonUtils/CommonUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CommonUtils 
+{
+
+    public unsafe static int GetSize<T>() where T : unmanaged
+    {
+        return sizeof(T);
+    }
+
+    public static int GetByteSize<T>(this List<T> list) where T : unmanaged
+    {
+        return GetSize<T>() * list.Count;
+    }
+
+    public static int GetByteSize<T>(this Array list) where T : unmanaged
+    {
+        return GetSize<T>() * list.Length;
+    }
+
+    public static int GetByteSize<T>()
+    {
+        return System.Runtime.InteropServices.Marshal.SizeOf(typeof(T));
+    }
+
+    public const float Btye2Mb = 1f / (1024f * 1024f);
+}
