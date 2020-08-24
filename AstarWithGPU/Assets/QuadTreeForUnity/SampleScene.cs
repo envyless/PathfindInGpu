08f6eee3daa695db801ec1dccdaecd2ad27ff544
envyless <yym4789@hotmail.com>
2020-08-24 17:17:52 +0900

    [feature] QuadTree Added

---
+++ AstarWithGPU/Assets/QuadTreeForUnity/SampleScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //init size
+        QuadTreeNode<int>.Init(Vector3.zero, Vector2.one * 100, 100);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var pos = ScreenToWorldPlane.GetWorldPlanePos();
+            QuadTreeNode<int>.Root.Insert(pos, (int)pos.x);
+        }        
+    }
+}
---
+++ AstarWithGPU/Assets/QuadTreeForUnity/Scripts/QuadViewer.cs
@@ -0,0 +1,32 @@
+﻿#define IS_Y_AXIS_HEIGHT
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuadViewer : MonoBehaviour
+{
+    private TextMeshPro tmp;
+    private MeshRenderer mr;
+    object quadTree;
+
+    private void Awake()
+    {       
+        tmp = GetComponentInChildren<TextMeshPro>();     
+        mr = GetComponentInChildren<MeshRenderer>();
+        mr.material.color = new Color(Random.Range(0,1f), Random.Range(0, 1f), Random.Range(0, 1f), 0.3f);
+    }
+
+    public void SetQuadTree<T>(QuadTreeNode<T> qtn)
+    {
+        quadTree = qtn;
+        mr.transform.localScale = new Vector3(qtn.boundary.width, qtn.boundary.height, 1);
+        var newPos = qtn.boundary.center;// - qtn.boundary.size * 0.5f;
+#if IS_Y_AXIS_HEIGHT
+        transform.position = new Vector3(newPos.x, newPos.y);
+#else
+        transform.position = new Vector3(newPos.x, 0, newPos.y);
+#endif
+        tmp.text = qtn.data.ToString();
+    }
+}
